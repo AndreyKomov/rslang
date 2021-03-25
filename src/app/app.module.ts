bf7698b34adfb1b803c8eb5890340d67452df48b
Daniil <uaolmer2@gmail.com>
2021-03-25 14:31:27 +0200

    feat: add header component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,14 @@ import AudioCallService from './pages/audio-call/audio-call.service';
 
 import MaterialModule from './pages/material/material.module';
 import AudioCallModule from './pages/audio-call/audio-call.module';
+import { HeaderComponent } from './pages/header/header.component';
+import { HeaderMenuComponent } from './pages/header/header-menu/header-menu.component';
+import { HeaderAuthComponent } from './pages/header/header-auth/header-auth.component';
+import { HeaderUserMenuComponent } from './pages/header/header-user-menu/header-user-menu.component';
+import { HeaderLogoComponent } from './pages/header/header-logo/header-logo.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent, HeaderMenuComponent, HeaderAuthComponent, HeaderUserMenuComponent, HeaderLogoComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AudioCallModule],
   providers: [AudioCallService],
   bootstrap: [AppComponent],
