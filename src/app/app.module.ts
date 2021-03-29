26da80ef83c9cc1830c5e865af19b97ffe38f7ab
Maria Titova <mary.titova.1995@gmail.com>
2021-03-29 15:51:40 +0400

    feat: add links to games

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import AppComponent from '@app/app.component';
+import GamePageComponent from './pages/game-page/game-page.component';
 
 import AudioCallService from './pages/audio-call/audio-call.service';
 
@@ -9,7 +10,7 @@ import MaterialModule from './pages/material/material.module';
 import AudioCallModule from './pages/audio-call/audio-call.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, GamePageComponent],
   imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AudioCallModule],
   providers: [AudioCallService],
   bootstrap: [AppComponent],
