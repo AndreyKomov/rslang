54b9cbf9c2bd32577746058cf27d43a5e6ce50a4
Maria Titova <mary.titova.1995@gmail.com>
2021-04-02 20:37:29 +0400

    feat: add routing to game-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,1 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'game-page',
+    loadChildren: (): any =>
+      import('./pages/game-page/game-page.module').then((m) => m.GamePageModule),
+  },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [],
+})
 export default class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import AppComponent from '@app/app.component';
-import GamePageComponent from './pages/game-page/game-page.component';
-
 import AudioCallService from './pages/audio-call/audio-call.service';
 
 import MaterialModule from './pages/material/material.module';
 import AudioCallModule from './pages/audio-call/audio-call.module';
+import AppRoutingModule from './app-routing.module';
 
 @NgModule({
-  declarations: [AppComponent, GamePageComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModule, AudioCallModule],
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    AudioCallModule,
+    AppRoutingModule,
+  ],
   providers: [AudioCallService],
   bootstrap: [AppComponent],
 })
