b75d21f77ce97cbb50ee4c72b2e264fcd7850c8b
Your Name <darki@tut.by>
2021-04-03 13:33:20 +0300

    fix:code  in app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,13 @@ import { HttpClientModule } from '@angular/common/http';
 import AppComponent from '@app/app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import ServerTestingComponent from './server-testing/server-testing.component';
 import WordsApiService from './server/api';
 import RegistrationComponent from './pages/registration/registration/registration.component';
 import AppRoutingModule from './app-routing.module';
 import SharedModule from './shared/shared.module';
 
 @NgModule({
-  declarations: [AppComponent, RegistrationComponent, ServerTestingComponent],
+  declarations: [AppComponent, RegistrationComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
