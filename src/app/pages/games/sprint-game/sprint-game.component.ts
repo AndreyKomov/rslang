53de19ea36c42a98964b505f8886c8ce094458ba
Your Name <darki@tut.by>
2021-04-04 23:47:41 +0300

    feat:shuffle word-translation

--- src/app/pages/games/sprint-game/sprint-game.component.ts
+++ src/app/pages/games/sprint-game/sprint-game.component.ts
@@ -14,16 +14,24 @@ display = false;
 level=0;
 round=0;
 wordsList:object|null;
+words:object|null;
+translations:object|null;
 
 constructor(public api: WordApiServiceComponent) {}
 ngOnInit(): void {
   this.getWords()
- 
 }
 
 update(){
    this.display = !this.display;
 }
+
+shuffle (array)  {
+  return array.sort(function() {
+      return 0.5 - Math.random();
+  });
+};
+
 onLevelChange(level){
 this.level=  Number(level)-1;
 this.getWords()
@@ -32,19 +40,28 @@ onRoundChange(round){
   this.round=  Number(round)-1;
   this.getWords()
   }
+createWordsArray = (wordsList) => wordsList.map((item) => item.word);
+createTranscriptionArray =  (wordsList) => wordsList.map((item) => item.wordTranslate);
 getWords(){
   this.api.getWordsByPageAndGroup(this.round, this.level).subscribe((data) => {
     this.wordsList = data;
-    console.log(this.wordsList)
+    console.log(this.wordsList);
+    this.wordsList=this.shuffle( this.wordsList);
+    this.words=this.createWordsArray(this.wordsList);
+    this.words=this.shuffle(this.words);
+    this.translations=this.createTranscriptionArray(this.wordsList);
+    this.translations=this.shuffle(this.translations);
+    console.log(this.words);
+    console.log(this.translations);
   });
-}
-/* 
-getWords() {
 
-  this.wordsList = await learnWordsAPIService.getWordsByPageAndGroup(this.round, this.level);
-  shuffle(this.wordsList);
-  this.words = createWordsArray(this.wordsList);
-  this.translations = createTranslationsArray(this.wordsList);
-} */
+}
+
+setLevelAndGroup() {
+  const level=this.level + 1;
+  const round=this.round + 1;
+  localStorage.setItem('level', level.toString());
+  localStorage.setItem('round',round.toString());
+}
 
 }
