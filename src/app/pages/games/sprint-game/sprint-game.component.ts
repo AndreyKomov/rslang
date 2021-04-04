fab8390a1087b25373da8aa040cc0b23fceaf803
Your Name <darki@tut.by>
2021-04-04 20:40:31 +0300

    feat:add range and level buttons

--- src/app/pages/games/sprint-game/sprint-game.component.ts
+++ src/app/pages/games/sprint-game/sprint-game.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import WordApiServiceComponent from '../../../server/api';
 
 
 @Component({
@@ -8,11 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./sprint-game.component.scss']
 })
 
-export class SprintGameComponent {
- message: string = "DataModule";
+export class SprintGameComponent implements OnInit {
+message: string = "DataModule";
 display = false;
+level=0;
+round=0;
+getWordsByPageAndGroup:object|null;
+
+constructor(public api: WordApiServiceComponent) {}
+ngOnInit(): void {
+  this.api.getWordsByPageAndGroup(this.round, this.level).subscribe((data) => {
+    this.getWordsByPageAndGroup = data;
+    console.log(this.getWordsByPageAndGroup)
+  });
+ 
+}
+
 update(){
    this.display = !this.display;
 }
+/* 
+getWords() {
+
+  this.wordsList = await learnWordsAPIService.getWordsByPageAndGroup(this.round, this.level);
+  shuffle(this.wordsList);
+  this.words = createWordsArray(this.wordsList);
+  this.translations = createTranslationsArray(this.wordsList);
+} */
 
 }
