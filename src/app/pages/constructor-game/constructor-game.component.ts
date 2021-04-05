1a155b2d362eebbec6ce5d0b8f13d6377cd48cbe
Konstantin <KostyaEK2006@gmail.com>
2021-04-05 18:53:27 +0300

    feat: add dont know button with keyboard event enter

--- src/app/pages/constructor-game/constructor-game.component.ts
+++ src/app/pages/constructor-game/constructor-game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-constructor-game',
@@ -10,7 +10,7 @@ export class ConstructorGameComponent implements OnInit {
   placeIndex = 0;
   word = 'car';
 
-  letterArr: String[];
+  letterArr: string[];
 
   rightLettersArr = [
     {
@@ -27,15 +27,15 @@ export class ConstructorGameComponent implements OnInit {
     },
   ];
 
-  sliceWord(word: string): String[] {
+  sliceWord(word: string): string[] {
     return word.split('');
   }
 
-  randomiseLetters(letterArr: String[]): String[] {
+  randomiseLetters(letterArr: string[]): string[] {
     return letterArr.sort();
   }
 
-  getReadyForGameWord(word: string): String[] {
+  getReadyForGameWord(word: string): string[] {
     let letterArr = this.sliceWord(word);
     return this.randomiseLetters(letterArr);
   }
@@ -61,5 +61,23 @@ export class ConstructorGameComponent implements OnInit {
 
   ngOnInit(): void {
     this.letterArr = this.getReadyForGameWord(this.word);
+  }
+
+  showAnswer(): void {
+    for (let i = 0; i < this.word.length; i++) {
+      this.rightLettersArr[i].letter = this.word[i];
+      this.rightLettersArr[i].isRight = true;
+    }
+
+    this.placeIndex = this.letterArr.length;
+
+    this.letterArr = [];
+  }
+
+  @HostListener('window:keydown', ['$event'])
+  showAnswerKeydownHandler(event: KeyboardEvent): void {
+    if (event.code === 'Enter') {
+      this.showAnswer();
+    }
   }
 }
