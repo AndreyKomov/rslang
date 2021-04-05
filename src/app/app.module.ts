8cb95c3a354acf6246db0cd47cdd8aac69e29efd
GoldenkovVitali <vitalik.resscuer@mail.ru>
2021-04-05 11:45:25 +0300

    init: start create savanna-game

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import SharedModule from './shared/shared.module';
   declarations: [AppComponent, RegistrationComponent],
   imports: [
     BrowserModule,
+    
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
