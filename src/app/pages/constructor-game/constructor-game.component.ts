273a2d5d47932f399b4506882f23b25cf8cdf000
Yulia Kravtsova <KostyaEK2006@gmail.com>
2021-04-06 23:17:08 +0300

    feat: add all functional for game

--- src/app/pages/constructor-game/constructor-game.component.ts
+++ src/app/pages/constructor-game/constructor-game.component.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener, OnInit } from '@angular/core';
+import WordsApiServiceComponent from '@app/server/api';
 
 @Component({
   selector: 'app-constructor-game',
@@ -6,26 +7,15 @@ import { Component, HostListener, OnInit } from '@angular/core';
   styleUrls: ['./constructor-game.component.scss'],
 })
 export class ConstructorGameComponent implements OnInit {
-  translateWord = 'Машина';
+  baseImgUrl = 'https://raw.githubusercontent.com/GoldenkovVitali/rslang-data/master/';
+  isEndRaund = false;
+  context = '';
+  translateWord = '';
   placeIndex = 0;
-  word = 'car';
-
+  word = '';
+  raund = 0;
   letterArr: string[];
-
-  rightLettersArr = [
-    {
-      letter: '',
-      isRight: false,
-    },
-    {
-      letter: '',
-      isRight: false,
-    },
-    {
-      letter: '',
-      isRight: false,
-    },
-  ];
+  rightLettersArr = [];
 
   sliceWord(word: string): string[] {
     return word.split('');
@@ -43,10 +33,11 @@ export class ConstructorGameComponent implements OnInit {
   checkLetter(letter: string, index: number, event: Event) {
     if (letter == this.word[this.placeIndex]) {
       this.letterArr.splice(index, 1);
-      this.rightLettersArr[this.placeIndex].letter = letter;
-      this.rightLettersArr[this.placeIndex].isRight = true;
-      console.log(this.rightLettersArr);
+      this.rightLettersArr[this.placeIndex] = letter;
       this.placeIndex++;
+      if (this.placeIndex === this.word.length) {
+        this.endRaund();
+      }
     } else {
       (<HTMLElement>event.target).classList.add('error');
       (<HTMLElement>event.target).classList.remove('ready-letter');
@@ -57,27 +48,52 @@ export class ConstructorGameComponent implements OnInit {
     }
   }
 
-  constructor() {}
+  constructor(private apiService: WordsApiServiceComponent) {}
 
   ngOnInit(): void {
-    this.letterArr = this.getReadyForGameWord(this.word);
+    this.nextRaund();
+  }
+
+  nextRaund(): void {
+    this.isEndRaund = false;
+    this.context = '';
+    this.translateWord = '';
+    this.placeIndex = 0;
+    this.word = '';
+    this.letterArr = [];
+    this.rightLettersArr = [];
+    this.apiService.getWordsByPageAndGroup(1, 1).subscribe(data => {
+      this.word = data[this.raund].word;
+      this.translateWord = data[this.raund].wordTranslate;
+      this.context = data[this.raund].textExample;
+      this.baseImgUrl += data[this.raund].image;
+      for (let i = 0; i < this.word.length; i++) {
+        this.rightLettersArr.push('');
+      }
+      this.letterArr = this.getReadyForGameWord(this.word);
+      console.log(data);
+    });
   }
 
   showAnswer(): void {
     for (let i = 0; i < this.word.length; i++) {
-      this.rightLettersArr[i].letter = this.word[i];
-      this.rightLettersArr[i].isRight = true;
+      this.rightLettersArr[i] = this.word[i];
     }
 
     this.placeIndex = this.letterArr.length;
 
     this.letterArr = [];
+    
+    this.endRaund();
   }
 
   @HostListener('window:keydown', ['$event'])
   showAnswerKeydownHandler(event: KeyboardEvent): void {
-    if (event.code === 'Enter') {
-      this.showAnswer();
-    }
+    event.code === 'Enter' && this.isEndRaund ? this.nextRaund() : this.showAnswer();
+  }
+
+  endRaund() {
+    this.raund++;
+    this.isEndRaund = true;
   }
 }
