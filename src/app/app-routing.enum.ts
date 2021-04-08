3eb7c839565d7607ece7b04e2829b96f4f3f2282
ANDREYKOMOV <komov.te@gmail.com>
2021-04-09 02:06:18 +0300

    fix: fix header, footer and main styles

--- src/app/app-routing.enum.ts
+++ src/app/app-routing.enum.ts
@@ -1,8 +1,10 @@
 export enum ParamKey {
   promo = '',
   notFound = '**',
+  textBook = 'textbook',
   team = 'team',
   games = 'games',
+  statistic = 'statistic',
   audiocallPromo = 'audiocall',
   audiocallGame = 'audiocallgame'
 }
