06e8a1f5f228d43d2fb962841fe7a0de719947f5
Konstantin Kravtsov <KostyaEK2006@gmail.com>
2021-04-10 19:20:45 +0300

    feat: add some changes

--- src/app/pages/registration/services/registration.service.ts
+++ src/app/pages/registration/services/registration.service.ts
@@ -1,13 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
-import WordsApiServiceComponent from '../../../server/api';
 import { IFileModel } from '../models/FileModel';
 
 import { IUserDataModel } from '../models/userDataModel';
 
 @Injectable()
 export default class RegistrationService {
-  constructor(private apiService: WordsApiServiceComponent) {}
+  private apiUrl = 'https://powerful-river-87536.herokuapp.com/';
+  userName: string;
+  email: string;
+  password: string;
+  avatar: IFileModel[];
+
+  constructor(private httpClient: HttpClient) {}
 
   singUp(name: string, password: string, email: string, imgPath: IFileModel[]): void {
     const newFile =
@@ -20,13 +26,32 @@ export default class RegistrationService {
             }
           )
         : imgPath[0];
-    this.apiService.createUser(name, email, password, newFile).subscribe((res: string) => res);
+    this.userName = name;
+    this.email = email;
+    this.password = password;
+    this.avatar = imgPath;
+    const data = new FormData();
+    data.append('avatar', newFile);
+    data.append('name', name);
+    data.append('email', email);
+    data.append('password', password);
+    this.httpClient.post<any>(`${this.apiUrl}users`, data).subscribe((res: string) => res);
   }
 
   logIn(password: string, email: string): void {
-    this.apiService.signIn(email, password).subscribe((res: IUserDataModel) => {
-      localStorage.setItem('token', res.token);
-      window.location.reload();
-    });
+    this.email = email;
+    this.password = password;
+    const data = {
+      email,
+      password,
+    };
+    this.httpClient
+      .post<any>(`${this.apiUrl}signin`, data, {
+        headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
+      })
+      .subscribe((res: IUserDataModel) => {
+        localStorage.setItem('token', res.token);
+        window.location.reload();
+      });
   }
 }
