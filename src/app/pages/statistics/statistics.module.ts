a11c4868195e3b2520e20afed801490e7d7934d9
Maria Titova <mary.titova.1995@gmail.com>
2021-04-12 14:33:02 +0400

    feat: add second chart without data

--- src/app/pages/statistics/statistics.module.ts
+++ src/app/pages/statistics/statistics.module.ts
@@ -4,9 +4,10 @@ import { ChartsModule } from 'ng2-charts';
 import StatisticsRoutingModule from './statistics-routing.module';
 import { StatisticsComponent } from './statistics.component';
 import { StatisticChartComponent } from './statistic-chart/statistic-chart.component';
+import { CommonStatisticChartComponent } from './common-statistic-chart/common-statistic-chart.component';
 
 @NgModule({
   imports: [CommonModule, StatisticsRoutingModule, ChartsModule],
-  declarations: [StatisticsComponent, StatisticChartComponent],
+  declarations: [StatisticsComponent, StatisticChartComponent, CommonStatisticChartComponent],
 })
 export class StatisticsModule {}
